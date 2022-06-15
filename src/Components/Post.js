8bb61a03d079691e403f25c3c5659d5aede72065
Risav Singh <risavsingh7462@gmail.com>
2022-06-15 11:43:29 +0530

    InstagramCloneReact-15th-June-2022-11:43-am

---
+++ src/Components/Post.js
@@ -0,0 +1,19 @@
+// rfce
+import React from "react";
+
+function post() {
+  return (
+    <div>
+
+      {/* header--> avatar + username */}
+
+      {/* image */}
+
+      {/* username + caption */}
+      
+      {/* 23min5sec */}
+    </div>
+  );
+}
+
+export default post;
